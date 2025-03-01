6f88453b7758a9faf31b601fda3abbf8ef90baa0
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-02 05:17:03 +0530

    Update iam.tf

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -25,6 +25,12 @@ resource "aws_iam_role_policy_attachment" "lambda_dynamodb_attach" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
+resource "aws_iam_role_policy_attachment" "lambda_cloudwatch_attach" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
+
 output "lambda_exec_role_arn" {
   value = aws_iam_role.lambda_exec.arn
 }
