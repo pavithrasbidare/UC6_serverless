54240c0f28ee9d3a8b3aaeb1b9c660712ad75980
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-02 11:47:58 +0530

    Update variables.tf

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,11 +1,13 @@
 variable "region" {
   description = "The AWS region to deploy resources"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "table_name" {
   description = "The name of the DynamoDB table"
   type        = string
+  default     = "UserTable"
 }
 
 variable "function_name" {
