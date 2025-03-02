343fe94f9d42a9ef112f6ef1b6bf83b1e4dcee49
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-02 10:56:09 +0530

    Update variables.tf

--- modules/api_gateway/variables.tf
+++ modules/api_gateway/variables.tf
@@ -1,5 +1,10 @@
-variable "lambda_function_arn" {
-  description = "The ARN of the Lambda function"
+variable "lambda_function_invoke_arn" {
+  description = "The invoke ARN of the Lambda function"
+  type        = string
+}
+
+variable "lambda_function_name" {
+  description = "The name of the Lambda function"
   type        = string
 }
 
