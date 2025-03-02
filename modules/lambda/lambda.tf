69d7ec61a44e6cbb868ae08c6f4cfc8629f92fab
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-02 10:40:35 +0530

    Update lambda.tf

--- modules/lambda/lambda.tf
+++ modules/lambda/lambda.tf
@@ -8,6 +8,10 @@ resource "aws_lambda_function" "lambda_function" {
   source_code_hash = filebase64sha256("${path.module}/lambda_function.zip")
 }
 
-output "lambda_function_arn" {
-  value = aws_lambda_function.lambda_function.arn
+output "lambda_function_invoke_arn" {
+  value = aws_lambda_function.lambda_function.invoke_arn
+}
+
+output "lambda_function_name" {
+  value = aws_lambda_function.lambda_function.function_name
 }
