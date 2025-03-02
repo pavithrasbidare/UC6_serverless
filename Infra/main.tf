0eefbb7e796c44c54d637275570059d0fe868c1a
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-02 10:57:37 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -20,7 +20,8 @@ module "lambda" {
 }
 
 module "api_gateway" {
-  source              = "../modules/api_gateway"
-  lambda_function_arn = module.lambda.lambda_function_arn
-  region              = var.region
+  source                      = "../modules/api_gateway"
+  lambda_function_invoke_arn  = module.lambda.lambda_function_invoke_arn
+  lambda_function_name        = module.lambda.lambda_function_name
+  region                      = var.region
 }
