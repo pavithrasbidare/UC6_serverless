6060b59a9a21c17f2acfa32de05a743e7f497727
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-03 10:19:29 +0530

    Update main.tf

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -18,7 +18,7 @@ resource "aws_api_gateway_resource" "user_id_resource" {
 resource "aws_api_gateway_method" "user_get_method" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.user_id_resource.id
-  http_method   = "GET"
+  http_method   = "ANY"
   authorization = "NONE"
 }
 
