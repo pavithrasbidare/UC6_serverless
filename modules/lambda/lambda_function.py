18e8e25a57425b6461592b446cea8b865e25697d
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-03 06:09:13 +0000

    updated code

--- modules/lambda/lambda_function.py
+++ modules/lambda/lambda_function.py
@@ -1,29 +1,29 @@
 import json
 import boto3
-import uuid
 
 def lambda_handler(event, context):
+    print("Event: ", json.dumps(event))  # Log the event object for debugging
     table_name = 'UserTable'
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(table_name)
- 
-    result = None
-    people = [
-        { 'userid' : 'marivera', 'name' : 'Martha Rivera'},
-        { 'userid' : 'nikkwolf', 'name' : 'Nikki Wolf'},
-        { 'userid' : 'pasantos', 'name' : 'Paulo Santos'},
-        { 'userid' : 'Mani', 'name' : 'Mani kanta'}
-    ]
- 
-    with table.batch_writer() as batch_writer:
-        for person in people:
-            item = {
-                'UserId'  : person['userid'],
-                'FullName': person['name']
+    
+    if 'httpMethod' in event and event['httpMethod'] == 'GET':
+        if 'pathParameters' in event and 'userId' in event['pathParameters']:
+            user_id = event['pathParameters']['userId']
+            response = table.get_item(Key={'UserId': user_id})
+            item = response.get('Item', {})
+            
+            return {
+                'statusCode': 200,
+                'body': json.dumps(item)
             }
-            print("> batch writing: {}".format(person['userid']) )
-            batch_writer.put_item(Item=item)
-           
-        result = f"Success. Added {len(people)} people to {table_name}."
- 
-    return {'message': result}
+        else:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'message': 'Missing userId in path parameters'})
+            }
+
+    return {
+        'statusCode': 400,
+        'body': json.dumps({'message': 'Unsupported method'})
+    }
